66a46b55fd2e55f907edbd9ec26d34329e369bd6
Cesar Tzaj <cesar.tzaj@outlook.com>
2023-01-22 21:02:44 -0600

    se creo el proyecto

---
+++ src/main/java/com/Maket/Market/Web/Controller/ProductController.java
@@ -0,0 +1,15 @@
+
+package com.Maket.Market.Web.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @GetMapping("/prueba")
+    public String a(){
+        return "asf";
+    }
+}
