cec7dbe0b061593bdadd09fec1e9878bb3589928
Cesar Tzaj <cesar.tzaj@outlook.com>
2023-01-26 13:24:00 -0600

    se agrego swagger http://localhost:8090/platzi-market/v1/swagger-ui/index.html

--- src/main/java/com/Maket/Market/MarketApplication.java
+++ src/main/java/com/Maket/Market/MarketApplication.java
@@ -2,7 +2,9 @@ package com.Maket.Market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 //@SpringBootApplication(scanBasePackages={"com.platzi.market.persistence.mapper.ProductMapper"})
 public class MarketApplication {
